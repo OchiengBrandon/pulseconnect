97806f4a668aabd5d3da66b07770e802284350a9
brandondeveloper <brandonochieng72@gmail.com>
2025-04-07 02:59:27 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,11 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run database migrations
 python manage.py migrate
-
-# Start Gunicorn
-gunicorn pulseconnect.wsgi:application --bind 0.0.0.0:8000
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
