d25b883fa41be50d0201be32d12ecd942d58ab38
brandondeveloper <brandonochieng72@gmail.com>
2025-04-07 02:47:12 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run database migrations
 python manage.py migrate
+
+# Start Gunicorn
+gunicorn pulseconnect.wsgi:application --bind 0.0.0.0:8000
