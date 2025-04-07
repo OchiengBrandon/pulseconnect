ce4cf093e4da81efa674cce8ad3147205067ec6c
brandondeveloper <brandonochieng72@gmail.com>
2025-04-07 03:18:24 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run database migrations
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+
+# Check if the superuser should be created
+if [[ $CREATE_SUPERUSER ]]; then
+  # Check if the superuser already exists
+  if ! python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists())" | grep -q "True"; then
+    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME"
+  else
+    echo "Superuser already exists."
+  fi
 fi
+
+# Start the application with Gunicorn
+exec gunicorn pulseconnect.wsgi:application --bind 0.0.0.0
