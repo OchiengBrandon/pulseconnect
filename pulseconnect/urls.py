9f77892312cf3e851dc030dc8671a18fca532798
brandondeveloper <brandonochieng72@gmail.com>
2025-04-07 12:25:41 +0300

    Done

--- pulseconnect/urls.py
+++ pulseconnect/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     # path('api/', include('api.urls')),
     path('dashboard/', TemplateView.as_view(template_name="home.html"), name='dashboard'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
+if settings.DEBUG:  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
